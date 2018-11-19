07210e7eb3acd611176e81d38c900d61764a44d9
Alexey <aleks9517@rambler.ru>
2018-11-19 05:03:24 +0200

    add ViewModelSport

---
+++ Notebook/ViewModels/ViewModelSport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Notebook.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notebook.ViewModels
+{
+    public class ViewModelSport
+    {
+        public List<string> Trainings { get; set; } 
+        public List<int> Sports { get; set; }
+    }
+}
